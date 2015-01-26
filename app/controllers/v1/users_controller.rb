5870f7877d5bae475fbb5a2394fdffc6babca325
Amy Yu <amycmyu@gmail.com>
2015-01-26 17:38:57 -0500

    added user registration

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -1,8 +1,39 @@
-module V1
-  class UsersController < ApplicationController
-    def index
-      users = {id: 1, email: 'xxx@xxx.com'}
-      render json: users, status: 200
+class V1::UsersController < ApplicationController
+  before_action :authenticate_with_token!, only: [:update, :destroy]
+  respond_to :json
+
+  def show
+    respond_with User.find(params[:id]) 
+  end
+
+  def create
+    user = User.new(user_params) 
+    if user.save
+      render json: user, status: 201, location: [:api, user] 
+    else
+      render json: { errors: user.errors }, status: 422
     end
   end
+
+  def update
+    user = current_user
+
+    if user.update(user_params)
+      render json: user, status: 200, location: [:api, user] 
+    else
+      render json: { errors: user.errors }, status: 422
+    end
+  end
+
+  def destroy
+	User.find(params[:id]).destroy
+    #current_user.destroy
+    head 204
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation, :name) 
+    end
 end
