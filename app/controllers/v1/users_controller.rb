329ab9f19af549f34c7703cf3c36f06e30825385
Amy Yu <amycmyu@gmail.com>
2015-02-01 22:54:52 -0500

    fixed bug that prevented create user to display json message

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -9,7 +9,7 @@ class V1::UsersController < ApplicationController
   def create
     user = User.new(user_params) 
     if user.save
-      render json: user, status: 201, location: [:api, user] 
+      render json: user, status: 201#, location: [:api, user] 
     else
       render json: { errors: user.errors }, status: 422
     end
@@ -19,7 +19,7 @@ class V1::UsersController < ApplicationController
     user = current_user
 
     if user.update(user_params)
-      render json: user, status: 200, location: [:api, user] 
+      render json: user, status: 200#, location: [:api, user] 
     else
       render json: { errors: user.errors }, status: 422
     end
@@ -34,6 +34,6 @@ class V1::UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation, :name) 
+      params.require(:user).permit(:email, :password, :password_confirmation, :user_name) 
     end
 end
