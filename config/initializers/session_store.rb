b0939b41f4f89d88f6b062a965e9533b675f7bd4
Likun <hu.likun@gmail.com>
2015-02-08 22:49:46 -0500

    disabled cookie generation in rails. All authentication is done via authentication token.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_feapp_session'
+# Rails.application.config.session_store :cookie_store, key: '_feapp_session'
+
+# For API server, Rails application should not issue session cookies but authentication should be done exclusively via the authentication token.
+Rails.application.config.session_store :disabled
